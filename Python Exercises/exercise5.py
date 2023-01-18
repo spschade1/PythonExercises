b390dd8863ee6bf682151396e3566f1d6dc1afd2
spschade1 <sleeper09@gmail.com>
2023-01-17 21:14:07 -0500

    Update exercise5.py
    Solved

--- Python Exercises/exercise5.py
+++ Python Exercises/exercise5.py
@@ -1,2 +1,14 @@
 #Exercise 5 - Fizz Buzz
 
+def fizzBuzz(upTo):
+    for number in range(1, upTo + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print('FizzBuzz', end = '')
+        elif number % 3 == 0:
+            print('Fizz', end = '')
+        elif number % 5 == 0:
+            print("Buzz", end = '')
+        else:
+            print(number, end = '')
+
+fizzBuzz(25)
